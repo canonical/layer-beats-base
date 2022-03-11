3a33b81f933a0c54821e4b4a0e8d9ba01947d1ee
Adam Dyess <adam.dyess@canonical.com>
2022-03-11 10:12:49 -0600

    Update style, linting, and github actions to run on pull requests

--- reactive/beats_base.py
+++ reactive/beats_base.py
@@ -1,88 +1,96 @@
-from charms.layer import status
-from charms.reactive import when
-from charms.reactive import when_not
-from charms.reactive import set_state
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+"""Base reactive file for beats charms."""
 from charmhelpers.core.unitdata import kv
+from charms.layer import status
+from charms.reactive import set_state, when, when_not
 
 
-@when('config.changed')
+@when("config.changed")
 def config_changed():
-    set_state('beat.render')
+    """Handle config changes."""
+    set_state("beat.render")
 
 
-@when_not('logstash.available',
-          'elasticsearch.available',
-          'kafka.ready',
-          'config.set.logstash_hosts',
-          'config.set.kafka_hosts')
+@when_not(
+    "logstash.available",
+    "elasticsearch.available",
+    "kafka.ready",
+    "config.set.logstash_hosts",
+    "config.set.kafka_hosts",
+)
 def waiting_messaging():
-    status.waiting('Waiting for: elasticsearch, logstash or kafka.')
+    """Set status when waiting on relations."""
+    status.waiting("Waiting for: elasticsearch, logstash or kafka.")
 
 
-@when('logstash.available')
+@when("logstash.available")
 def cache_logstash_data(logstash):
+    """Use unit database to cache connected hosts until beat is available."""
     units = logstash.list_unit_data()
     cache = kv()
-    if cache.get('beat.logstash'):
-        hosts = cache.get('beat.logstash')
+    if cache.get("beat.logstash"):
+        hosts = cache.get("beat.logstash")
     else:
         hosts = []
     for unit in units:
-        host_string = "{0}:{1}".format(unit['private_address'],
-                                       unit['port'])
+        host_string = "{0}:{1}".format(unit["private_address"], unit["port"])
         if host_string not in hosts:
             hosts.append(host_string)
 
-    cache.set('beat.logstash', hosts)
-    set_state('beat.render')
+    cache.set("beat.logstash", hosts)
+    set_state("beat.render")
 
 
-@when_not('logstash.available')
+@when_not("logstash.available")
 def cache_remove_logstash_data():
+    """Remove cache no longer needed now that logstash relation is gone."""
     cache = kv()
-    cache.unset('beat.logstash')
-    set_state('beat.render')
+    cache.unset("beat.logstash")
+    set_state("beat.render")
 
 
-@when('elasticsearch.available')
+@when("elasticsearch.available")
 def cache_elasticsearch_data(elasticsearch):
+    """Use unit database to cache connected hosts until beat is available."""
     units = elasticsearch.list_unit_data()
     cache = kv()
     hosts = []
     for unit in units:
-        host_string = "{0}:{1}".format(unit['host'],
-                                       unit['port'])
+        host_string = "{0}:{1}".format(unit["host"], unit["port"])
         if host_string not in hosts:
             hosts.append(host_string)
 
-    cache.set('beat.elasticsearch', hosts)
-    set_state('beat.render')
+    cache.set("beat.elasticsearch", hosts)
+    set_state("beat.render")
 
 
-@when_not('elasticsearch.available')
+@when_not("elasticsearch.available")
 def cache_remove_elasticsearch_data():
+    """Remove cache no longer needed now that elasticsearch relation is gone."""
     cache = kv()
-    cache.unset('beat.elasticsearch')
-    set_state('beat.render')
+    cache.unset("beat.elasticsearch")
+    set_state("beat.render")
 
 
-@when('kafka.ready')
+@when("kafka.ready")
 def cache_kafka_data(kafka):
+    """Handle when Kafka is related."""
     units = kafka.kafkas()
     cache = kv()
     hosts = []
     for unit in units:
-        host_string = "{0}:{1}".format(unit['host'],
-                                       unit['port'])
+        host_string = "{0}:{1}".format(unit["host"], unit["port"])
         if host_string not in hosts:
             hosts.append(host_string)
 
-    cache.set('beat.kafka', hosts)
-    set_state('beat.render')
+    cache.set("beat.kafka", hosts)
+    set_state("beat.render")
 
 
-@when_not('kafka.ready')
+@when_not("kafka.ready")
 def cache_remove_kafka_data():
+    """Handle when Kafka is departed."""
     cache = kv()
-    cache.unset('beat.kafka')
-    set_state('beat.render')
+    cache.unset("beat.kafka")
+    set_state("beat.render")
